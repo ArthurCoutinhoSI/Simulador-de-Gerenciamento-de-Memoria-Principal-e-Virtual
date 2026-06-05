24173b677012bac2af259a64c101ce8b8087de09
ArthurCoutinhoSI <arthurrcouti@gmail.com>
2026-06-05 01:35:28 -0400

    implement hashtable on PageTable

--- src/pagetable/PageTable.java
+++ src/pagetable/PageTable.java
@@ -1,43 +1,31 @@
 package pagetable;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 public class PageTable {
-	// Implementado como um mapa de número de página virtual -> PageTableObject
-	private final Map<Integer, PageTableObject> pageTable;
-	private final int capacidade;
+	// tabela hash do diretorio
+	PageTableObject[] pageTable;
+	int capacidade;
 
-	public PageTable(int capacidade) {
-		this.pageTable = HashMap.newHashMap(capacidade);
-		this.capacidade = capacidade;
+	public PageTable(int n){
+		this.pageTable = new PageTableObject[n];
+		this.capacidade = n;
 	}
 
-	public void put(int virtualPageNumber, PageTableObject obj) {
-		pageTable.put(virtualPageNumber, obj);
+	public void add(int index, int frame){
+		pageTable[index].setFrame(frame);
+		pageTable[index].setEstaPresente(true);
 	}
 
-	public PageTableObject get(int virtualPageNumber) {
-		return pageTable.get(virtualPageNumber);
+	public int softRemove(int index){
+		pageTable[index].setEstaPresente(false);
+		return pageTable[index].getFrame();
 	}
 
-	public boolean contains(int virtualPageNumber) {
-		return pageTable.containsKey(virtualPageNumber);
-	}
-
-	// faz o softdelete.
-	public PageTableObject removeByframe(PageTableObject ) {
-		PageTableObject pto = pageTable.containsValue();
-		pto.setEstaPresente(false);
-		return pto;
-	}
-
-	public int size() {
-		return pageTable.size();
-	}
-
-	public int getCapacidade(){
-		return this.capacidade;
+	public int findIndexOfFrame(int frame) {
+		for (int i = 0; i < pageTable.length; i++) {
+			if(pageTable[i].getFrame() == frame){
+				return i;
+			}
+		}
+		return -1;
 	}
 }
