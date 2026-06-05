339b1927b9c87fd4cc9b7eead8c9067a6c3183e5
ArthurCoutinhoSI <arthurrcouti@gmail.com>
2026-06-05 01:36:29 -0400

    clear strategies

--- src/estrategiassubstituicaopaginas/estrategias/EstrategiaFifo.java
+++ src/estrategiassubstituicaopaginas/estrategias/EstrategiaFifo.java
@@ -8,35 +8,10 @@ import pagetable.PageTable;
 import pagetable.PageTableObject;
 
 public class EstrategiaFifo implements EstrategiaSubstituicaoPagina {
-    ArrayList<PageTable> fila;
+    ArrayList<int> fila;
 
 	public EstrategiaFifo(){
 		fila = new ArrayList<>();
 	}
-
-	public void acessarAdicionarOuSubstituirPagina(PageTable pageTable, int paginaIndex) throws Exception{
-        if (pageTable == null || paginaIndex < 0) {
-            throw new Exception("pageTable == null || paginaIndex < 0");
-        }
-
-        if (this.fila.contains(paginaIndex)) {
-            // não faz nada
-			return;
-        }
-
-		//  getframelivre
-		int frame = -1;
-		PageTableObject aux = new PageTableObject(frame, true);
-
-		if(pageTable.size() == pageTable.getCapacidade()){
-			fila.add(frame);
-			pageTable.put(paginaIndex, aux);
-			return;
-		}
-
-		pageTable.remove(fila.remove(0).);
-		fila.add(frame);
-
-		return;
-	}
+	
 }
--- src/estrategiassubstituicaopaginas/interfaces/EstrategiaSubstituicaoPagina.java
+++ src/estrategiassubstituicaopaginas/interfaces/EstrategiaSubstituicaoPagina.java
@@ -3,5 +3,6 @@ package estrategiassubstituicaopaginas.interfaces;
 import pagetable.PageTable;
 
 public interface EstrategiaSubstituicaoPagina {
-    public void acessarAdicionarOuSubstituirPagina(PageTable pageTable, int frame) throws Exception;
+    public void add(int frame);
+    public int remove();
 }
